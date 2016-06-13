e7a1713947acc7bd60e2252e62f6ee50565f4429
Shinji <shinji51@hotmail.fr>
2016-06-13 19:07:08 +0200

    modification général et implémentation d'effet

--- js/script.js
+++ js/script.js
@@ -1,62 +1,8 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-
-        // //Navigation
-        // menu: '#menu',
-        // lockAnchors: false,
-        // anchors:['firstPage', 'secondPage'],
-        // navigation: false,
-        // navigationPosition: 'right',
-        // navigationTooltips: ['firstSlide', 'secondSlide'],
-        // showActiveTooltip: false,
-        // slidesNavigation: true,
-        // slidesNavPosition: 'bottom',
-
-        // //Scrolling
-        css3: true,
-        scrollingSpeed: 1700,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        // scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        // loopBottom: false,
-        // loopTop: false,
-        // loopHorizontal: true,
-        // continuousVertical: false,
-        normalScrollElements: null,
-        // scrollOverflowHandler: 'iscrollHandler',
-        scrollOverflow: true,
-        scrollOverflowOptions: null,
-        // touchSensitivity: 15,
-        // normalScrollElementTouchThreshold: 5,
-
-        // //Accessibility
-        // keyboardScrolling: true,
-        // animateAnchor: true,
-        // recordHistory: true,
-
-        // //Design
-        // controlArrows: true,
-        // verticalCentered: true,
-        sectionsColor : ['black', '#1F8E99', 'red'],
-        // paddingTop: '3em',
-        // paddingBottom: '10px',
-        // fixedElements: '#header, .footer',
-        // responsiveWidth: 0,
-        responsiveHeight: 0,
-
-        // //Custom selectors
-        // sectionSelector: '.section',
-        // slideSelector: '.slide',
-
-        // //events
-        // onLeave: function(index, nextIndex, direction){},
-        // afterLoad: function(anchorLink, index){},
-        // afterRender: function(){},
-        // afterResize: function(){},
-        // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        // onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-    });
+	$(function() {
+		$("#one").hide(0).delay(500).show(3000);
+		$("#two").hide(0).delay(2000).show(3000);
+		$("#three").hide(0).delay(5000).show(3000);
+		$("#four").hide(0).delay(8000).show(3000);
+	});
 });
