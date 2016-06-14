57410414109d95113e20262565dc712ae4f1a48a
Shinji <shinji51@hotmail.fr>
2016-06-14 18:31:47 +0200

    ajout de de margin et paddin sur div two pour régler bug affichage de la photo

--- js/script.js
+++ js/script.js
@@ -4,5 +4,7 @@ $(document).ready(function() {
 		$("#two").hide(0).delay(2000).show(3000);
 		$("#three").hide(0).delay(5000).show(3000);
 		$("#four").hide(0).delay(8000).show(3000);
+		$("#five").hide(0).delay(11000).show(3000);
+
 	});
 });
